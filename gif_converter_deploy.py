612b71607dfbfae4557760e07ce501e82fc68f84
nakyeong-kim <nana20170207@gmail.com>
2023-10-22 16:38:02 +0900

    open source project 2

--- gif_converter_deploy.py
+++ gif_converter_deploy.py
@@ -1,4 +1,4 @@
-# [OSS project] PyPI 배포
+# [OSS project] PyPI, Gihub 배포
 
 # PyPI, build, package deploy
 
@@ -14,7 +14,7 @@ c.convert_gif()
 
 
 """
-패키지 배포 순서(PyPI)
+패키지 배포 순서 : PyPI
 
 1. http://pypi.org 회원가입
 2. 프로젝트 구조 확인 (하기 3, 4번에 해당하는 내용들)
@@ -35,6 +35,30 @@ c.convert_gif()
 6. PyPI 배포
    [설치] pip install twine
    [배포] python -m twine upload dist/*
-7. 설치 확인
+7. 설치해보기
+   - pip install gif-converter-nk → 가상환경/Lib/site-packages/에 gif-converter-nk 디렉토리 생성되는 것 확인
+   - 사용법 : from gif_converter_nk.gif_converter import GifConverter
+              c = GifConverter('./project/images/*.png', './project/image_out/result.gif', (320, 240))
+              c.convert_gif()
+
+"""
+
+"""
+패키지 배포 순서 : Gihub
+
+1. https://github.com 회원가입
+2. git 설치 확인 (.gitignore 파일 고려)
+3. git add → commit → push 
+   - git repository 저장소 생성
+   - git init
+   - git status
+   - git add .
+   - git status
+   - git commit -m 'message'
+   - git remote add origin https://github.com/nakyeong-kim/gif_converter_nk.git
+   - git push origin main
+4. PyPI 형태의 패키지 구조를 github repository에 push
+5. 설치해보기
+   - pip install git+https://github.com/nakyeong-kim/gif_converter_nk.git
 
 """
