75c52676a5cc9a4f0fe2bbbae7c8478fb5c36e78
nakyeong-kim <nana20170207@gmail.com>
2024-04-01 14:23:02 +0900

    최종 정리

---
+++ last.py
@@ -0,0 +1,22 @@
+"""
+
+* 중요 개념
+    - Thread란?
+    - MultiThread란?
+    - Context Switching이란?
+    - Mutext, Semaphore, Monitor란?
+    - Garbage Collector란?
+    - Python, Java, Golang, JavaScript 등 대표 언어 공부
+    - Computer CS 지식, 운영체제 지식
+
+
+* 앞으로
+    1. 코드에 신경쓸 것 : 코드의 의도, 네이밍, 원활한 유지보수가 가능한 코드, 정확한 코드, 정돈된 코드
+       ※ Pycodestyle, Autopep8, Pylint 패키지 활용
+    2. 코드 적게 쓰기 : 간결/논리적 표현, 라인은 돈이 든다
+    3. 오픈소스 참여 : 코드 줄여 개선, 문제점 토론, 내 실력 공개
+    4. 디버깅(테스트 위주로 개발) : 예상치 못한 예외 or 오류 or 에러
+    5. 숙련도가 쌓이면 급격하게 성장 : 트렌디하게 기술 흐름에 대한 관심 필요
+       ※ Github → Trend Ranking 참고
+
+"""
